81e28009efde4d5a0483bb8978c7c3fdcec15ad7
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-29 02:28:38 +0300

    inject-fields fixup for precision and limits

--- build/inject-fields.js
+++ build/inject-fields.js
@@ -156,7 +156,7 @@ function searchFor (filename) {
                 sectionLines.length = sectionLines.length - 1
                 const indent = sectionLines[sectionLines.length - 1].match (/^\s+/)[0].length
                 for (const missingKey of toAdd) {
-                    sectionLines.push (' '.repeat (indent) + '\'' + missingKey + '\': undefined,')
+                    sectionLines.push (' '.repeat (indent) + '\'' + missingKey + '\': ' + (missingKey === 'precision' || missingKey === 'limits' ? ('this.' + missingKey) : 'undefined') + ',')
                 }
                 sectionLines.push (end)
                 toInject.push ({top, bottom, sectionLines})
